a15111a4505c7b033847050324dc503c103b2c53
Emil <emilwagenplast@gmail.com>
2023-01-16 16:36:28 +0100

    release.sh: fix variables

--- release.sh
+++ release.sh
@@ -14,12 +14,13 @@ METADATA=$(unzip -p "$ZIPPATH"/$FILENAME META-INF/com/android/metadata)
 DEVICE=$(echo "$METADATA" | grep pre-device | cut -f2 -d '=' | cut -f1 -d ',')
 DATETIME=$(echo "$METADATA" | grep post-timestamp | cut -f2 -d '=')
 ID=$(cut -f1 -d ' ' "$ZIPPATH"/${FILENAME}.sha256)
-ROMTYPE=$(echo "$ZIPPATH"/$FILENAME | cut -f4 -d '-')
-ROMNAME=$(echo "$ZIPPATH"/$FILENAME | cut -f1 -d '-')
-JSON="$BUILDS/${ROMNAME,,}/$ROMTYPE/${DEVICE}.json"
+ROMTYPE=$(echo $FILENAME | cut -f4 -d '-')
+ROMVARIANT=$(echo $FILENAME | cut -f5 -d '-')
+ROMNAME=$(echo $FILENAME | cut -f1 -d '-')
+JSON="$BUILDS/${ROMNAME,,}/$ROMVARIANT/${DEVICE}.json"
 SIZE=$(du -b "$ZIPPATH"/$FILENAME | cut -f1 -d '	')
-VERSION=$(echo "$ZIPPATH"/$FILENAME | cut -f2 -d '-')
-DATE=$(echo "$ZIPPATH"/${FILENAME%.*} | cut -f6 -d '-')
+VERSION=$(echo $FILENAME | cut -f2 -d '-')
+DATE=$(echo ${FILENAME%.*} | cut -f6 -d '-')
 TAG=BlissROM_$VERSION-$DATE
 URL="$REPO/releases/download/$TAG/$FILENAME"
 
@@ -31,7 +32,7 @@ echo "size": $SIZE,
 echo "url": "$URL",
 echo "version": "$VERSION"
 
-mv "$ZIPPATH"/$FILENAME "$BUILDS"/${ROMNAME,,}/$ROMTYPE/ && mv "$ZIPPATH"/${FILENAME}.sha256 "$BUILDS"/${ROMNAME,,}/$ROMTYPE/
+mv "$ZIPPATH"/$FILENAME "$BUILDS"/${ROMNAME,,}/$ROMVARIANT/ && mv "$ZIPPATH"/${FILENAME}.sha256 "$BUILDS"/${ROMNAME,,}/$ROMVARIANT/
 /bin/cat <<EOM >$JSON
 {
   "response": [
@@ -50,9 +51,9 @@ EOM
 
 # Push update to GitHub
 uploadbuild(){
-  git -C "$BUILDS" add ${ROMNAME,,}/$ROMTYPE/raphael.json ${ROMNAME,,}/$ROMTYPE/changelog.md
+  git -C "$BUILDS" add ${ROMNAME,,}/$ROMVARIANT/raphael.json ${ROMNAME,,}/$ROMVARIANT/changelog.md
   git -C "$BUILDS" commit -m raphael_$DATE
-  cd "$BUILDS"/ && gh release create $TAG -F ${ROMNAME,,}/$ROMTYPE/changelog.md ${ROMNAME,,}/$ROMTYPE/$FILENAME --target master && git -C "$BUILDS" push origin master
+  cd "$BUILDS"/ && gh release create $TAG -F ${ROMNAME,,}/$ROMVARIANT/changelog.md ${ROMNAME,,}/$ROMVARIANT/$FILENAME --target master && git -C "$BUILDS" push origin master
   echo "Build is released!"
 }
 releaseprompt(){
@@ -60,7 +61,7 @@ releaseprompt(){
   read -r -p "Do you want to upload the build? [y/N] " response
   case "$response" in
       [yY][eE][sS]|[yY])
-          nano "$BUILDS"/${ROMNAME,,}/$ROMTYPE/changelog.md
+          nano "$BUILDS"/${ROMNAME,,}/$ROMVARIANT/changelog.md
           uploadbuild
           ;;
       *)

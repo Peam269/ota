a315c02d2fa8b87d7a65bd942c0e51f57bb0a54a
Emil <emilwagenplast@gmail.com>
2023-05-27 20:34:18 +0200

    update releasescript

--- release.sh
+++ release.sh
@@ -5,10 +5,12 @@ echo "Where is your ROM source-code located at? (please type the full path)"
 read ROMPATH
 else ROMPATH=$1; fi
 REPO=https://github.com/Peam269/ota
+UPLOAD=https://gitlab.com/Peam269/ota
 BUILDS=~/ota
 ZIPPATH="$ROMPATH"/out/target/product/raphael
+if [ ! -d "$BUILDS/upload/" ]; then git clone $UPLOAD "$BUILDS"/upload; fi
 if [ ! -d "$BUILDS/old/" ]; then mkdir "$BUILDS"/old; fi
-if [ -n "$(ls -A "$BUILDS"/*/*/*.zip)" ]; then mv "$BUILDS"/*/*/*.zip "$BUILDS"/old/ && mv "$BUILDS"/*/*/*.sha256 "$BUILDS"/old/; fi
+if [ -n "$(ls -A "$BUILDS"/upload/*/*/*.zip)" ]; then mv "$BUILDS"/upload/*/*/*.zip "$BUILDS"/old/; fi > /dev/null 2>&1
 FILENAME=$(ls "$ZIPPATH"/Bliss-*.zip | tail -n1 | xargs -n1 basename)
 METADATA=$(unzip -p "$ZIPPATH"/$FILENAME META-INF/com/android/metadata)
 DEVICE=$(echo "$METADATA" | grep pre-device | cut -f2 -d '=' | cut -f1 -d ',')
@@ -22,7 +24,7 @@ SIZE=$(du -b "$ZIPPATH"/$FILENAME | cut -f1 -d '	')
 VERSION=$(echo $FILENAME | cut -f2 -d '-')
 DATE=$(echo ${FILENAME%.*} | cut -f6 -d '-')
 TAG=BlissROM_$VERSION-$ROMVARIANT-$DATE
-URL="$REPO/releases/download/$TAG/$FILENAME"
+URL="$UPLOAD/raw/master/${ROMNAME,,}/$ROMVARIANT/$FILENAME"
 
 echo "datetime": $DATETIME,
 echo "filename": "$FILENAME",
@@ -32,8 +34,8 @@ echo "size": $SIZE,
 echo "url": "$URL",
 echo "version": "$VERSION"
 
-if [ ! -d "$BUILDS"/${ROMNAME,,} ]; then mkdir "$BUILDS"/${ROMNAME,,}; fi
-if [ ! -d "$BUILDS"/${ROMNAME,,}/$ROMVARIANT ]; then mkdir "$BUILDS"/${ROMNAME,,}/$ROMVARIANT; fi
+if [ ! -d "$BUILDS"/${ROMNAME,,} ]; then mkdir "$BUILDS"/${ROMNAME,,}; fi && if [ ! -d "$BUILDS"/upload/${ROMNAME,,} ]; then mkdir "$BUILDS"/upload/${ROMNAME,,}; fi
+if [ ! -d "$BUILDS"/${ROMNAME,,}/$ROMVARIANT ]; then mkdir "$BUILDS"/${ROMNAME,,}/$ROMVARIANT; fi && if [ ! -d "$BUILDS"/upload/${ROMNAME,,}/$ROMVARIANT ]; then mkdir "$BUILDS"/upload/${ROMNAME,,}/$ROMVARIANT; fi
 /bin/cat <<EOM >$JSON
 {
   "response": [
@@ -52,11 +54,13 @@ EOM
 
 # Push update to GitHub
 uploadbuild(){
-  mv "$ZIPPATH"/$FILENAME "$BUILDS"/${ROMNAME,,}/$ROMVARIANT/ && mv "$ZIPPATH"/${FILENAME}.sha256 "$BUILDS"/${ROMNAME,,}/$ROMVARIANT/
+  mv "$ZIPPATH"/$FILENAME "$BUILDS"/upload/${ROMNAME,,}/$ROMVARIANT/
 
   git -C "$BUILDS" add ${ROMNAME,,}/$ROMVARIANT/raphael.json ${ROMNAME,,}/$ROMVARIANT/changelog.md
   git -C "$BUILDS" commit -m raphael_$DATE-$ROMVARIANT
-  cd "$BUILDS"/ && gh release create $TAG -F ${ROMNAME,,}/$ROMVARIANT/changelog.md ${ROMNAME,,}/$ROMVARIANT/$FILENAME --target master && git -C "$BUILDS" push origin master
+  git -C "$BUILDS"/upload add ${ROMNAME,,}/$ROMVARIANT
+  git -C "$BUILDS"/upload commit --amend --no-edit
+  git -C "$BUILDS"/upload push -f origin master && git -C "$BUILDS" push origin master
   echo "Build is released!"
 }
 releaseprompt(){
